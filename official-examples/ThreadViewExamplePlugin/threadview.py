59fe25414bf2d68f3097148de6edd6601116f22e
Gaute Hope <eg@gaute.vetsj.com>
2018-07-01 11:40:52 +0200

    use webkit 2

--- official-examples/ThreadViewExamplePlugin/threadview.py
+++ official-examples/ThreadViewExamplePlugin/threadview.py
@@ -2,7 +2,7 @@ import gi
 gi.require_version ('Astroid', '0.1')
 gi.require_version ('Astroid', '0.1')
 gi.require_version ('Gtk', '3.0')
-gi.require_version ('WebKit', '3.0')
+gi.require_version ('WebKit', '4.0')
 from gi.repository import Gtk
 from gi.repository import WebKit
 from gi.repository import GObject, Astroid
